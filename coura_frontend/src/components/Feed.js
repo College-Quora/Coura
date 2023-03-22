7f3574405b1e8b7c0e0b8a12ff3bc8887c966430
ayushigupta931 <ayushigupta931@gmail.com>
2023-03-23 01:43:10 +0530

    updates

--- coura_frontend/src/components/Feed.js
+++ coura_frontend/src/components/Feed.js
@@ -14,7 +14,7 @@ function Feed({ searchKey, category, setCategory }) {
 
   useEffect(() => {
     axios
-      .get("/api/questions")
+      .get("/questions")
       .then((res) => {
         console.log(res.data);
         setPosts(res.data.reverse());
@@ -36,7 +36,7 @@ function Feed({ searchKey, category, setCategory }) {
     };
 
     axios
-      .post("/api/questions/votes", body, config)
+      .post("/questions/votes", body, config)
       .then((res) => {
         console.log(res.data.votes);
         setVotes(res.data.votes);
