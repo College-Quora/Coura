0cbb7993a3ee2213d1299e74aff06450e1cf891d
gunjan-g <gunjang1202@gmail.com>
2023-03-22 19:38:21 +0530

    updated website

--- coura_frontend/src/index.js
+++ coura_frontend/src/index.js
@@ -11,7 +11,7 @@ TimeAgo.addDefaultLocale(en);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename='/'>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
