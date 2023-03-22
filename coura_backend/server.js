75b16140b1249db4d6aaddcdc07bf9820cbeaa14
ayushigupta931 <ayushigupta931@gmail.com>
2023-03-22 20:53:45 +0530

    updates

--- coura_backend/server.js
+++ coura_backend/server.js
@@ -28,24 +28,24 @@ app.use("/api", router);
 
 //app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
 
-app.use(express.static(path.join(__dirname, "/../coura_frontend/build")));
-app.get("*", (req, res) => {
-  try {
-    res.sendFile(path.join(__dirname, "/../coura_frontend/build/index.html"));
-  } catch (e) {
-    res.send("Oops! unexpected error!");
-  }
-});
+// app.use(express.static(path.join(__dirname, "/../coura_frontend/build")));
+// app.get("*", (req, res) => {
+//   try {
+//     res.sendFile(path.join(__dirname, "/../coura_frontend/build/index.html"));
+//   } catch (e) {
+//     res.send("Oops! unexpected error!");
+//   }
+// });
 
 
-// app.use(express.static(path.resolve(__dirname, '../coura_frontend', 'build')));
-//     app.get("/*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, '../coura_frontend', 'build', 'index.html'),function (err) {
-//             if(err) {
-//                 res.status(500).send(err)
-//             }
-//         });
-//     });
+app.use(express.static(path.resolve(__dirname, 'coura_frontend', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'coura_frontend', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    });
 
     /*
     app.use(express.static(path.join(__dirname, "../coura_frontend/build")));
